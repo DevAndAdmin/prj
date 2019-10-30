f6fe6d51bb6ce432f90e39d40824590c2e7d7d0c
DevAndAdmin <evgenij.yurtaev.87@mail.ru>
2019-10-30 19:30:02 +0400

    first step

---
+++ sharp_less1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sharp_less1
+{
+    class gpu
+    {
+        string name;
+        string ram;
+        int cores;
+        float freq;
+        public string Name {
+            get {
+                return name;
+            }
+        }
+        public gpu(){
+            name = "";
+            ram = "";
+            cores = 0;
+            calc();
+        }
+        public gpu(string name, string ram, int cores)
+        {
+            this.name = name;
+            this.ram = ram;
+            this.cores = cores;
+            calc();
+        }
+        public void calc() {
+            freq = cores * 5;
+        }
+    }
+
+    class s_gpu : gpu {
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            gpu vcard = new gpu("rtx2080ti", "12Gb", 4000);
+           // vcard.name = "rtx 2080ti";
+           // vcard.cores = 4000;
+            label1.Text = vcard.Name;
+        }
+    }
+}
